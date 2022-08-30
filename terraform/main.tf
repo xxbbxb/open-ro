da9dd54ba391b6dc2072fc1342b15dcd4966d76a
xxbbxb <simonovbs@gmail.com>
2022-08-30 23:36:42 +0300

    remove default firewall rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,13 +16,13 @@ provider "civo" {
 }
 
 
-resource "civo_firewall" "openro-firewall" {
-    name = "openro-firewall"
+resource "civo_firewall" "open-ro-firewall" {
+    name = "open-ro-firewall"
     create_default_rules = false
 }
 
 resource "civo_firewall_rule" "openro-kubeapi" {
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     protocol = "tcp"
     start_port = "6443"
     end_port = "6443"
@@ -33,7 +33,7 @@ resource "civo_firewall_rule" "openro-kubeapi" {
 }
 
 resource "civo_firewall_rule" "tcp-egress-allow" {
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     protocol = "tcp"
     start_port = "1"
     end_port = "65535"
@@ -44,7 +44,7 @@ resource "civo_firewall_rule" "tcp-egress-allow" {
 }
 
 resource "civo_firewall_rule" "udp-egress-allow" {
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     protocol = "udp"
     start_port = "1"
     end_port = "65535"
@@ -55,7 +55,7 @@ resource "civo_firewall_rule" "udp-egress-allow" {
 }
 
 resource "civo_firewall_rule" "icmp-ingress-allow" {
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     protocol = "icmp"
     cidr = ["0.0.0.0/0"]
     direction = "ingress"
@@ -64,7 +64,7 @@ resource "civo_firewall_rule" "icmp-ingress-allow" {
 }
 
 resource "civo_firewall_rule" "icmp-egress-allow" {
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     protocol = "icmp"
     cidr = ["0.0.0.0/0"]
     direction = "egress"
@@ -88,7 +88,7 @@ data "civo_size" "small" {
 
 resource "civo_kubernetes_cluster" "openro" {
     name = "openro"
-    firewall_id = civo_firewall.openro-firewall.id
+    firewall_id = civo_firewall.open-ro-firewall.id
     pools {
         label = "openro-worker"
         size = element(data.civo_size.small.sizes, 0).name
